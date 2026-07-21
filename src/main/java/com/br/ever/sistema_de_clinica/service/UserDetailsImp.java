d169cc005ece4fb543b6c709cbee165f315c7649
Everson Ramos Gomes <eversonramos2015@gmail.com>
2026-07-21 17:25:01 -0300

    Criação de UserDetailsService

--- src/main/java/com/br/ever/sistema_de_clinica/service/UserDetailsImp.java
+++ src/main/java/com/br/ever/sistema_de_clinica/service/UserDetailsImp.java
@@ -1,4 +1,19 @@
 package com.br.ever.sistema_de_clinica.service;
 
-public class UserDetailsImp {
+import com.br.ever.sistema_de_clinica.database.dominio.DoutorEntity;
+import com.br.ever.sistema_de_clinica.repository.IDoutorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsImp implements UserDetailsService {
+    private IDoutorRepository doutorRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return doutorRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+    }
 }
