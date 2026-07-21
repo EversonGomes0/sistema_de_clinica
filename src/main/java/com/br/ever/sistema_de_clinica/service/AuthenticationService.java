00b48db58ba72613a8aadc2ccf7e8d79c2bb94f9
Everson Ramos Gomes <eversonramos2015@gmail.com>
2026-07-21 17:25:20 -0300

    Criação de AuthenticationService

--- src/main/java/com/br/ever/sistema_de_clinica/service/AuthenticationService.java
+++ src/main/java/com/br/ever/sistema_de_clinica/service/AuthenticationService.java
@@ -1,4 +1,42 @@
 package com.br.ever.sistema_de_clinica.service;
 
 public class AuthenticationService {
+
+    private IPacienteRepository pacienteRepository;
+    private IDoutorRepository doutorRepository;
+    private IRolesRepository rolesRepository;
+    private SecurityConfiguration securityConfiguration;
+
+
+    public void cadastrarDoutor(RegisterDto registerDto) {
+        if (registerDto.getNome().isEmpty() || registerDto.getCpf().isEmpty()){
+            throw new RuntimeException("Doutor não possui nome ou cpf");
+        }
+
+        var roles = rolesRepository.findByName(RolesType.ROLE_ADMIN.name())
+                .orElseGet(() -> rolesRepository.save(RolesEntity.builder().nome(RolesType.ROLE_ADMIN.name()).build()));
+
+
+        doutorRepository.save(DoutorEntity.builder().nome(registerDto.getNome())
+                .cpf(registerDto.getCpf())
+                        .roles(Set.of(roles))
+                        .senha(securityConfiguration.passwordEncoder().encode(registerDto.getSenha()))
+                .build());
+    }
+
+    public void cadastrarPaciente(RegisterDto registerDto) {
+        if (registerDto.getNome().isEmpty() || registerDto.getCpf().isEmpty()){
+            throw new RuntimeException("Paciente não possui nome ou cpf");
+        }
+
+        var roles = rolesRepository.findByName(RolesType.ROLE_ADMIN.name())
+                .orElseGet(() -> rolesRepository.save(RolesEntity.builder().nome(RolesType.ROLE_ADMIN.name()).build()));
+
+
+        pacienteRepository.save(PacienteEntity.builder().nome(registerDto.getNome())
+                .cpf(registerDto.getCpf())
+                .roles(Set.of(roles))
+                .senha(securityConfiguration.passwordEncoder().encode(registerDto.getSenha()))
+                .build());
+    }
 }
